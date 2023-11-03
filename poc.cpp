a8c3a2992e923d502e5d778bfaeb6714ebf17e7f
Eduardo Costa <m4c0@users.noreply.github.com>
2023-11-03 08:51:15 +0000

    poc: removing reentrancy requirement

--- poc.cpp
+++ poc.cpp
@@ -2,12 +2,4 @@
 
 import curser;
 
-int main() {
-  auto guard = curser::hide();
-  // cursor is hidden
-  {
-    auto guard1 = curser::hide();
-    // nothing is done since there is another guard active
-  }
-  // cursor will be shown after this method ends
-}
+int main() { auto guard = curser::hide(); }
